bb8b91fb1e0c5f927fe703860a55458f9f8a375d
FoxLuc <lucaf8502@gmail.com>
2020-09-24 16:23:24 +0000

    01

---
+++ browserpai/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'browserpai';
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('http://4200-e74a1c1d-b84f-4c5e-863c-9c105c1f836d.ws-eu01.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
+}
